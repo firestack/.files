443081fa9de4879cf43080458f86b18614cd0033
Firestack <firestack@users.noreply.github.com>
2016-03-01 10:21:39 -0700

    Add USB Copier

---
+++ usbcopier.ps1
@@ -0,0 +1,4 @@
+﻿robocopy ../MOB $HOME/Source/MOB /MIR /xd .svn Binaries Build Intermediate Saved /xo /mon:50 /mot:10
+
+robocopy ../MOB "$($(Get-Volume -FileSystemLabel StackFire)[0].DriveLetter):/Source/MOB" /MIR /xd .svn Binaries Build Intermediate Saved /xo /mon:50 /mot:10
+
